a939869f1068dba08a74d7b81b687d45b8151ba0
chinna0107 <kancharlahemanth89@gmail.com>
2026-07-15 11:01:53 +0530

    Production 2.23

--- src/pages/Admin/TeluguNews.jsx
+++ src/pages/Admin/TeluguNews.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useOutletContext } from 'react-router-dom';
 import { Plus, Trash2, Edit, Check, X, Loader2, Eye, Calendar as CalendarIcon, FileText, Search } from 'lucide-react';
